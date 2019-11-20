8c7dbfd206bdd25a9b982bc6c749cbb85735d542
Yasushi Ikeda <yikeda6616@gmail.com>
2019-11-20 13:00:54 +0900

    Move autoprefixer browserslist from gulpfile to package.json

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -28,11 +28,7 @@ function css() {
     .pipe($.sourcemaps.init())
     .pipe($.sass())
     .on('error', $.sass.logError)
-    .pipe(
-      $.autoprefixer({
-        browsers: ['last 2 versions']
-      })
-    )
+    .pipe($.autoprefixer())
     .pipe($.sourcemaps.write())
     .pipe(dest('./dist/css'))
     .pipe(
