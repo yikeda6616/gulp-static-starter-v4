098b36b0c36b5c81c1e68f30b9c3051835cd0813
Yasushi Ikeda <yikeda6616@gmail.com>
2020-11-26 21:48:00 +0900

    refactor: Add paths to variable

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -2,8 +2,14 @@ const { src, dest, parallel, watch } = require('gulp');
 const $ = require('./modules.js');
 const uglify = $.composer($.uglifyes, $.composer);
 
+const path = {
+  src: './src',
+  dist: './dist'
+}
+
+
 function html() {
-  return src(['./src/pug/*.pug', '!./src/pug/**/_*.pug'])
+  return src([`${path.src}/pug/*.pug`, `!${path.src}/pug/**/_*.pug`])
     .pipe(
       $.plumber({
         errorHandler: $.notify.onError('Error: <%= error.message %>')
@@ -14,7 +20,7 @@ function html() {
         pretty: true
       })
     )
-    .pipe(dest('./dist'))
+    .pipe(dest(path.dist))
     .pipe(
       $.browserSync.reload({
         stream: true,
@@ -24,20 +30,20 @@ function html() {
 }
 
 function css() {
-  return src('./src/scss/*.scss')
+  return src(`${path.src}/scss/*.scss`)
     .pipe($.sourcemaps.init())
     .pipe($.sass())
     .on('error', $.sass.logError)
     .pipe($.autoprefixer())
     .pipe($.sourcemaps.write())
-    .pipe(dest('./dist/css'))
+    .pipe(dest(`${path.dist}/css`))
     .pipe(
       $.rename({
         suffix: '.min'
       })
     )
     .pipe($.minifyCSS())
-    .pipe(dest('./dist/css'))
+    .pipe(dest(`${path.dist}/css`))
     .pipe(
       $.browserSync.reload({
         stream: true,
@@ -47,7 +53,7 @@ function css() {
 }
 
 function js() {
-  return src('./src/js/*.js', { sourcemaps: true })
+  return src(`${path.src}/js/**/*.js`, { sourcemaps: true })
     .pipe($.plumber())
     .pipe(uglify({ output: { comments: /^!/ } }))
     .pipe(
@@ -55,7 +61,7 @@ function js() {
         newLine: '\n'
       })
     )
-    .pipe(dest('./dist/js', { sourcemaps: true }))
+    .pipe(dest(`${path.dist}/js`, { sourcemaps: true }))
     .pipe(
       $.browserSync.reload({
         stream: true,
@@ -65,20 +71,20 @@ function js() {
 }
 
 function img() {
-  return src('./src/img/**/**')
-    .pipe($.changed('./dist/img/'))
+  return src(`${path.src}/img/**/**`)
+    .pipe($.changed(`${path.dist}/img/`))
     .pipe(
       $.imagemin({
         optimizationLevel: 3
       })
     )
-    .pipe(dest('./dist/img/'));
+    .pipe(dest(`${path.dist}/img/`));
 }
 
 function bs() {
   $.browserSync.init({
     server: {
-      baseDir: './dist/'
+      baseDir: path.dist
     },
     notify: true,
     xip: false
@@ -92,8 +98,8 @@ exports.bs = bs;
 exports.img = img;
 
 exports.default = parallel([html, css, js, img, bs], () => {
-  watch('./src/pug/**', html);
-  watch('./src/scss/**', css);
-  watch('./src/js/**', js);
-  watch('./src/img/**', img);
+  watch(`${path.src}/pug/**`, html);
+  watch(`${path.src}/scss/**`, css);
+  watch(`${path.src}/js/**`, js);
+  watch(`${path.src}/img/**`, img);
 });
